8f4e95214404fd1fa5edb14df2388ec4dbc34f0a
Hankering1716 <116718014+Hankering1716@users.noreply.github.com>
2024-05-30 21:38:41 +0200

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,1 +1,29 @@
 
+
+const currentDate = document.querySelectorAll(".date");
+
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+
+const Data = new Date();
+
+document.querySelectorAll('.date').forEach(node => node.innerHTML = Data.getDate() + " " + month[Data.getMonth()] + " " + Data.getFullYear());
+
+
+
+
+
+// Get user country in order to translate site into his language (Not ready yet)
+
+
+// const regionNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'region' });
+// const regionNamesInTraditionalChinese = new Intl.DisplayNames(['zh-Hant'], {
+//   type: 'region',
+// });
+
+// console.log(regionNamesInEnglish.of('US'));
+// console.log(regionNamesInTraditionalChinese.of('US'));
+
+
+
+// document.getElementById("countryComputer").innerHTML =  regionNamesInEnglish.of(Intl.DateTimeFormat().resolvedOptions().timeZone) 
